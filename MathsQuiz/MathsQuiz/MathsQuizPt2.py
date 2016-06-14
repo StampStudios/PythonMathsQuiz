5e6ea0c2952b6954fdf6c986a60d85f66c3b41e3
ItsTenchy <jack@jtench.co.uk>
2016-06-14 22:11:12 +0100

    Added a new file for Task 2.

---
+++ MathsQuiz/MathsQuiz/MathsQuizPt2.py
@@ -0,0 +1,19 @@
+import random
+score = loop = 0
+while loop < 10:
+    n1 = random.randint(1, 10)  
+    n2 = random.randint(1, 10)
+    op_int = random.randint(1, 3)
+    if op_int == 1: ans = n1 + n2
+    elif op_int == 2:   ans = n1 - n2
+    else:    ans = n1 * n2
+    if op_int == 1: op = " + "
+    elif op_int == 2:   op = " - "
+    else:   op = " x "
+    while True:
+        try:    usrAns = int(input(str(n1) + op + str(n2) + " = "))
+        except ValueError:  continue
+        else:    break
+    if int(usrAns) == ans:  score += 1
+    loop += 1
+print("Well done you got a score of " + str(score) + "/10.")
