bf10cff26d1779a9f8b5e9503a62cdde0f1acf45
tomti <tomti@DESKTOP-M45J832>
2016-06-14 22:00:48 +0100

    crazy shit

--- MathsQuiz/MathsQuiz/MathsQuiz.py
+++ MathsQuiz/MathsQuiz/MathsQuiz.py
@@ -4,15 +4,12 @@ while loop < 10:
     n1 = random.randint(1, 10)
     n2 = random.randint(1, 10)
     op_int = random.randint(1, 3)
-    if op_int == 1:
-        ans = n1 + n2
-        op = " + "
-    elif op_int == 2:
-        ans = n1 - n2
-        op = " - "
-    else:
-        ans = n1 * n2
-        op = " x "
+    if op_int == 1: ans = n1 + n2
+    elif op_int == 2:   ans = n1 - n2
+    else:    ans = n1 * n2
+    if op_int == 1: op = " + "
+    elif op_int == 2:   op = " - "
+    else:   op = " x "
     while True:
         try:    usrAns = int(input(str(n1) + op + str(n2) + " = "))
         except ValueError:  continue
