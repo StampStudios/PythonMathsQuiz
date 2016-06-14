9f7a9830415b9294521821c64e63225f14ffa3a1
tomti <tomti@DESKTOP-M45J832>
2016-06-14 21:29:25 +0100

    Wiped out num1 and num2 population. Saved a line.

--- MathsQuiz/MathsQuiz/MathsQuiz.py
+++ MathsQuiz/MathsQuiz/MathsQuiz.py
@@ -1,34 +1,33 @@
 import random
 usrName = input("What is your name? ")
-score = 0
-loop = 0
+score = loop = 0
 while loop < 10:
     n1 = random.randint(1, 10)
     n2 = random.randint(1, 10)
     op_int = random.randint(1, 3)
     if op_int == 1:
-        ans = num1 + num2
+        ans = n1 + n2
         op = " + "
     elif op_int == 2:
-        ans = num1 - num2
+        ans = n1 - n2
         op = " - "
     else:
-        ans = num1 * num2
+        ans = n1 * n2
         op = " x "
     while True:
         try:
-            usrAns = int(input(str(num1) + op + str(num2) + " = "))
+            usrAns = int(input(str(n1) + op + str(n2) + " = "))
         except ValueError:
             print("That is not a valid answer")
             continue
         else:
             break
 
-        if int(usrAns) == ans:
-            print("Correct!")
-            score += 1
-        else:
+    if int(usrAns) == ans:
+        print("Correct!")
+        score += 1
+    else:
             print("Incorrect!")
-        loop += 1
+    loop += 1
 
-        print("Well done, " + name + " you got a score of " + str(score) + "/10.")
+print("Well done, " + usrName + " you got a score of " + str(score) + "/10.")
