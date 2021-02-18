9a05aeb852ad2e922140a768001a7d18463241e0
ThomasTinsley <34382191+ThomasTinsley@users.noreply.github.com>
2021-02-18 21:37:45 +0000

    Update MathsQuiz.py

--- MathsQuiz/MathsQuiz/MathsQuiz.py
+++ MathsQuiz/MathsQuiz/MathsQuiz.py
@@ -1,6 +1,6 @@
 import random
-score = loop = 0
-while loop < 10:
+score = 0
+for i in range(10):
     n1 = random.randint(1, 10)  
     n2 = random.randint(1, 10)
     op_int = random.randint(1, 3)
@@ -15,5 +15,4 @@ while loop < 10:
         except ValueError:  continue
         else:    break
     if int(usrAns) == ans:  score += 1
-    loop += 1
 print("Well done you got a score of " + str(score) + "/10.")
